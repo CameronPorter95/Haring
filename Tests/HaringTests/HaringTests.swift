f7ca530b4f44a3ab7d1f4d8751243a193432f34b
Cameron <cameronporter95@gmail.com>
2023-04-20 16:18:26 +1200

    Added swift package manager support

---
+++ Tests/HaringTests/HaringTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Haring
+
+final class HaringTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Haring().text, "Hello, World!")
+    }
+}
